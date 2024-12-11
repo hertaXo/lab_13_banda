6d03c1853c625711e4fb2206e965c01568257435
liaanausane <liana.usane@edu.rtu.lv>
2024-12-11 16:03:13 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,6 +7,15 @@
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 import java.util.*;
 // neaizmirstam pievienot konkrētās bibliotēkas, ko izmantojāt. Pagaidām pievienoju visas
 //import java.util.PriorityQueue;
